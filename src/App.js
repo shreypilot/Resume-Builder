ce2fba25d3b4eb60e7ad8fcc78748da44bb428ec
shreya kumar <shreypilot28@gmail.com>
2023-03-25 07:17:49 +0530

    resume builder part 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/Header/Header';
+import Body from './components/Body/Body';
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Body />
     </div>
   );
 }
